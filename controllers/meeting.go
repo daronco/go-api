1cc7ac1adbe04974b7bc073cfec2db8c0a8697d7
Leonardo Crauss Daronco <leonardodaronco@gmail.com>
2015-05-07 18:09:19 -0400

    Connect to redis pubsub and receive a "meeting created" message
    Just receives the message, parses into a struct and prints it.

--- controllers/meeting.go
+++ controllers/meeting.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	// "encoding/json"
-	"fmt"
 	"github.com/bigbluebutton/api-labs/models"
 
 	"github.com/astaxie/beego"
--- main.go
+++ main.go
@@ -5,12 +5,17 @@ import (
 	_ "github.com/bigbluebutton/api-labs/routers"
 
 	"github.com/astaxie/beego"
+	"github.com/bigbluebutton/api-labs/lib/redispubsub"
 )
 
 func main() {
 	if beego.RunMode == "dev" {
 		beego.DirectoryIndex = true
 		beego.StaticDir["/swagger"] = "swagger"
+
 	}
+
+	go redispubsub.Connect()
+
 	beego.Run()
 }
